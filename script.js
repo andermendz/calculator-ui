fb1614a63778422b360dd2f939d95b1b448caba2
andermendz <andersonmendz19@gmail.com>
2023-01-20 19:51:25 -0500

    added comments for later

---
+++ script.js
@@ -0,0 +1,9 @@
+//The first operation with start a empty array
+// the first operation will use 3 elements 
+//
+// 1st element -- first number
+// 2nd element -- evaluator substraction/ multiplication that will be used to define the kind of operation
+// 3rd element -- second number
+
+// after the first operation the result will took the place
+// of the first element and everything else will be processed as before
